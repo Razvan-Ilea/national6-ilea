f59cbc71b56ffafec3ba47381b3a14eb0c1a0e9b
Razvan Ilea <razvan_ilea12@yahoo.com>
2021-03-23 15:19:14 +0200

    finished homework

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
 console.log('To Do List - Homework');
+
+const container = document.getElementById('container')
+
+document.addEventListener('load', getData); 
+
+function getData() {
+    fetch('https://simple-json-server-scit.herokuapp.com/todo/rilea')
+        .then((r) => r.json())
+}
+
+
+const plus = document.getElementById("plus");
+
+  plus.addEventListener("click", function () {
+
+    const inputValue = document.getElementById("todo-input").value;
+    console.log(inputValue);
+
+    const taskContainer = document.createElement('div');
+    taskContainer.classList.add('task-div')
+
+    const task = document.createElement('p');
+    task.classList.add('task-content')
+
+    const checkedButton = document.createElement('input');
+    checkedButton.type = 'checkbox';
+
+    const deleteButton = document.createElement('img');
+    deleteButton.src = 'delete.png';
+    deleteButton.classList.add('delete-button');
+    deleteButton.addEventListener('click', function() {
+        taskContainer.remove();
+
+        fetch('https://simple-json-server-scit.herokuapp.com/todo/rilea', {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+             body: JSON.stringify(payload)
+        }).then(getData);
+    })
+    
+    container.appendChild(taskContainer);
+    
+    taskContainer.appendChild(checkedButton);
+    taskContainer.appendChild(task);
+    taskContainer.appendChild(deleteButton);
+
+    task.innerText = inputValue
+    
+    if (inputValue) {
+        const payload = {
+            id: 'rilea',
+            todo:[
+            {
+                checked: false,
+                item: inputValue
+            }
+        ]
+        }
+        fetch('https://simple-json-server-scit.herokuapp.com/todo', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+        }).then(getData);
+    }
+})
+
