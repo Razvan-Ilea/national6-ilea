530c0cff4353f0bed3dc68f9b79a267ab3a84b4e
Razvan Ilea <razvan_ilea12@yahoo.com>
2021-03-25 16:36:24 +0200

    finished homework

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 console.log('Spaceship Generator Homework');
+
+class Spaceship {
+    constructor() {
+        this.xPosition = 0;
+        this.yPosition = 0;
+        this.generateHtmlRef();
+        this.setMoveSpaceship()
+    }
+    generateHtmlRef() {
+        const spaceship = ['blue-spaceship.png','red-spaceship.png','green-spaceship.png']  
+        this.ref = document.createElement("img");
+        this.ref.src = spaceship[Math.floor(Math.random() * spaceship.length)];
+        this.ref.classList.add("spaceship");
+        document.body.appendChild(this.ref);
+    }
+    setMoveSpaceship() {
+        document.addEventListener("keydown", (event) => {
+            newCar.moveSpaceship(event.key)
+        });
+    }
+    moveSpaceship(direction) {
+        if (direction === "ArrowRight") {
+            this.xPosition += 5;
+            this.ref.style.transform = `translate(${this.xPosition}px, ${this.yPosition}px)`;
+        } else if (direction === "ArrowLeft") {
+            this.xPosition -= 5;
+            this.ref.style.transform = `translate(${this.xPosition}px, ${this.yPosition}px)`;
+        } else if (direction === "ArrowUp") {
+            this.yPosition -= 5;
+            this.ref.style.transform = `translate(${this.xPosition}px, ${this.yPosition}px)`;
+        } else if (direction === "ArrowDown") {
+            this.yPosition += 5;
+            this.ref.style.transform = `translate(${this.xPosition}px, ${this.yPosition}px)`;
+        }
+    }    
+};
+
+let newCar;
+document.getElementById('generate-spaceship').addEventListener('click', function() {
+    newCar = new Spaceship();
+})
