21851e828cb684b9704e881b0258f39d1657b62d
Razvan Ilea <razvan_ilea12@yahoo.com>
2021-04-22 20:57:23 +0300

    add item

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { Header } from "./components/Header/Header";
 
 import "./App.css";
-import { ToDoItem } from "./components/ToDoItem/ToDoItem";
-import { ToDoItemWithState } from "./components/ToDoItemWithState/ToDoItemWithState";
+import { ToDoList } from "./components/ToDoList/ToDoList";
 
 function App() {
   return (
     <div className="App" id="app">
       <Header />
-      <ToDoItem label="task 1" checkValue={false} />
-      <ToDoItem label="task 2" checkValue={true} />
-      <ToDoItem label="task 2" checkValue={true} />
-      <ToDoItemWithState label="task 4" checkValue={true} />
+      <ToDoList />
     </div>
   );
 }
--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -1,7 +1,6 @@
 import "./ToDoItem.css";
 
 export function ToDoItem(props) {
-  console.log(props);
   return (
     <div className="to-do-item">
       <input type="checkbox" defaultChecked={props.checkValue} />
@@ -9,6 +8,7 @@ export function ToDoItem(props) {
       <img
         src="https://www.flaticon.com/svg/vstatic/svg/748/748023.svg?token=exp=1618938211~hmac=e273bb6c449187bec21d242bf6382bce"
         alt="trash"
+        onClick={() => props.removeItem(props.label)}
       />
     </div>
   );
