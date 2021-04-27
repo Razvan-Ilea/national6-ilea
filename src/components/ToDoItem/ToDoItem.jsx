bf5225ab9ec344b313de2fd16257f601d6e2009d
Razvan Ilea <razvan_ilea12@yahoo.com>
2021-04-27 18:44:58 +0300

    add trash

--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -6,7 +6,7 @@ export function ToDoItem(props) {
       <input type="checkbox" defaultChecked={props.checkValue} />
       <p>{props.label}</p>
       <img
-        src="https://www.flaticon.com/svg/vstatic/svg/748/748023.svg?token=exp=1618938211~hmac=e273bb6c449187bec21d242bf6382bce"
+        src="https://image.freepik.com/free-icon/trash-bin-symbol_318-10194.jpg"
         alt="trash"
         onClick={() => props.removeItem(props.label)}
       />
