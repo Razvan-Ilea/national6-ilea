c0f900152913a579e1283695462c345be7b0ed67
Razvan Ilea <razvan_ilea12@yahoo.com>
2021-04-29 18:25:57 +0300

    todo with serer

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,58 +1,139 @@
 import { Component } from "react";
 import { ToDoItem } from "../ToDoItem/ToDoItem";
 
-const mockToDoListData = [
-  {
-    checked: false,
-    item: "task 1",
-  },
-  {
-    checked: true,
-    item: "task 2",
-  },
-  {
-    checked: false,
-    item: "task 3",
-  },
-];
-
 export class ToDoList extends Component {
   state = {
-    toDoList: mockToDoListData,
+    toDoList: [],
     inputValue: "",
+    shouldCreateUser: false,
+    isInvalid: false,
   };
 
   removeItem = (itemText) => {
+    const filteredToDoList = this.state.toDoList.filter(
+      (itemData) => itemData.item !== itemText
+    );
+
     this.setState({
-      toDoList: this.state.toDoList.filter(
-        (itemData) => itemData.item !== itemText
-      ),
+      toDoList: filteredToDoList,
+    });
+
+    const payload = {
+      id: "rilea",
+      todo: filteredToDoList,
+    };
+
+    fetch("https://simple-json-server-scit.herokuapp.com/todo/rilea", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
     });
   };
 
+  updateCheckStatus = (index, value) => {
+    console.log(index, value);
+
+    const newList = this.state.toDoList.map((item, itemIndex) => {
+      if (itemIndex === index) {
+        item.checked = value;
+      }
+
+      return item;
+    });
+
+    this.setState({ toDoList: newList });
+
+    const payload = {
+      id: "rilea",
+      todo: newList,
+    };
+
+    fetch("https://simple-json-server-scit.herokuapp.com/todo/rilea", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+  };
+
+  componentDidMount() {
+    console.log("ToDoList has finished mounting");
+    fetch("https://simple-json-server-scit.herokuapp.com/todo/rilea")
+      .then((r) => r.json())
+      .then((json) => {
+        console.log(json);
+        if (json.todo) {
+          this.setState({ toDoList: json.todo });
+        } else {
+          this.setState({ shouldCreateUser: true });
+        }
+      });
+  }
+
+  componentWillUnmount() {
+    console.log("ToDoList will be unmounted");
+  }
+
   handleInputChange = (event) => {
-    this.setState({ inputValue: event.target.value });
+    this.setState({ inputValue: event.target.value, isInvalid: false });
   };
 
   handleAddItem = () => {
-    this.setState({
-      toDoList: [
-        { checked: false, item: this.state.inputValue },
-        ...this.state.toDoList,
-      ],
-      inputValue: "",
-    });
+    if (this.state.inputValue === "") {
+      this.setState({ isInvalid: true });
+    } else {
+      this.setState({
+        toDoList: [
+          { checked: false, item: this.state.inputValue },
+          ...this.state.toDoList,
+        ],
+        inputValue: "",
+        isInvalid: false,
+      });
+
+      const payload = {
+        id: "rilea",
+        todo: [
+          { checked: false, item: this.state.inputValue },
+          ...this.state.toDoList,
+        ],
+      };
+
+      if (this.state.shouldCreateUser) {
+        fetch("https://simple-json-server-scit.herokuapp.com/todo", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        });
+        this.setState({ shouldCreateUser: false });
+      } else {
+        fetch("https://simple-json-server-scit.herokuapp.com/todo/rilea", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        });
+      }
+    }
   };
 
   render() {
     return (
       <div className="to-do-list">
-        {this.state.toDoList.map((itemData) => (
+        {this.state.toDoList.map((itemData, index) => (
           <ToDoItem
-            key={itemData.item}
+            key={index + itemData.item}
             checkValue={itemData.checked}
             label={itemData.item}
             removeItem={this.removeItem}
+            itemIndex={index}
+            updateCheckStatus={this.updateCheckStatus}
           />
         ))}
 
@@ -60,6 +141,9 @@ export class ToDoList extends Component {
           <input
             value={this.state.inputValue}
             onChange={this.handleInputChange}
+            style={{
+              border: this.state.isInvalid ? "4px solid red" : undefined,
+            }}
           />
           <button onClick={this.handleAddItem}>+</button>
         </div>
