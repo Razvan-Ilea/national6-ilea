7c00170dfab052815bb5191fa44a76bbed637ca3
Razvan Ilea <razvan_ilea12@yahoo.com>
2021-05-01 14:09:05 +0300

    finshed homework

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,19 +5,49 @@ import { SendButton } from "./SendButton";
 
 export class Form extends Component {
   state = {
-    inputValue: "",
-    isInvalid: false,
+    firstInput: "",
+    secondInput: "",
+    thirdInput: "",
+    messageInput: "",
+    firstInputInvalid: false,
+    secondInputInvalid: false,
+    thirdInputInvalid: false,
+    messageInputInvalid: false,
   };
 
-  handleInputChange = (event) => {
-    this.setState({ inputValue: event.target.value });
+  handleFirstInputChange = (event) => {
+    this.setState({ firstInput: event.target.value });
+  };
+  handleSecondInputChange = (event) => {
+    this.setState({ secondInput: event.target.value });
+  };
+  handleThirdInputChange = (event) => {
+    this.setState({ thirdInput: event.target.value });
+  };
+  handleMessageInputChange = (event) => {
+    this.setState({ messageInput: event.target.value });
   };
 
   handleSendButton = () => {
-    if (this.state.inputValue === "") {
-      this.setState({ isInvalid: true });
+    if (this.state.firstInput === "") {
+      this.setState({ firstInputInvalid: true });
     } else {
-      this.setState({ isInvalid: false });
+      this.setState({ firstInputInvalid: false });
+    }
+    if (this.state.secondInput === "") {
+      this.setState({ secondInputInvalid: true });
+    } else {
+      this.setState({ secondInputInvalid: false });
+    }
+    if (this.state.thirdInput === "") {
+      this.setState({ thirdInputInvalid: true });
+    } else {
+      this.setState({ thirdInputInvalid: false });
+    }
+    if (this.state.messageInput === "") {
+      this.setState({ messageInputInvalid: true });
+    } else {
+      this.setState({ messageInputInvalid: false });
     }
   };
 
@@ -27,34 +57,36 @@ export class Form extends Component {
       <div className="contact-form">
         <FormField
           label="FIRST NAME"
-          value={this.state.inputValue}
-          onChange={this.handleInputChange}
+          value={this.state.firstInput}
+          onChange={this.handleFirstInputChange}
           style={{
-            border: this.state.isInvalid ? "2px solid red" : undefined,
+            border: this.state.firstInputInvalid ? "2px solid red" : undefined,
           }}
         />
         <FormField
           label="LAST NAME"
-          value={this.state.inputValue}
-          onChange={this.handleInputChange}
+          value={this.state.secondInput}
+          onChange={this.handleSecondInputChange}
           style={{
-            border: this.state.isInvalid ? "2px solid red" : undefined,
+            border: this.state.secondInputInvalid ? "2px solid red" : undefined,
           }}
         />
         <FormField
           label="EMAIL"
-          value={this.state.inputValue}
-          onChange={this.handleInputChange}
+          value={this.state.thirdInput}
+          onChange={this.handleThirdInputChange}
           style={{
-            border: this.state.isInvalid ? "2px solid red" : undefined,
+            border: this.state.thirdInputInvalid ? "2px solid red" : undefined,
           }}
         />
         <FormMessage
           label="MESSAGE"
-          value={this.state.inputValue}
-          onChange={this.handleInputChange}
+          value={this.state.messageInput}
+          onChange={this.handleMessageInputChange}
           style={{
-            border: this.state.isInvalid ? "2px solid red" : undefined,
+            border: this.state.messageInputInvalid
+              ? "2px solid red"
+              : undefined,
           }}
         />
         <SendButton onClick={this.handleSendButton} />
